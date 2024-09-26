a461cfedaebb4aa0aaa5f2d8e15d56bbab1c3dc7
Fran <franrego333@gmail.com>
2024-09-26 11:34:55 +0200

    Ejercicio 9 completado

--- src/main/java/com/campusdual/classroom/Exercise09.java
+++ src/main/java/com/campusdual/classroom/Exercise09.java
@@ -3,7 +3,9 @@ package com.campusdual.classroom;
 public class Exercise09 {
 
     public static void main(String[] args) {
+        int Y = 5;
 
+        doWhileLoop(Y);
     }
 
     //TODO ↓
@@ -12,6 +14,16 @@ public class Exercise09 {
     // Entra con valor: X+1, sale con valor: X+2;
     // etc.
     public static void doWhileLoop(int num) {
+
+        int X = 0;
+
+        do {
+            
+            // Mostrar mensaje indicando con qué valor entra/ sale del bucle
+            System.out.println("Entra con valor: "+X+", sale con valor: "+(X+1));
+            X++;
+
+        } while (X < num);
 
     }
 }
